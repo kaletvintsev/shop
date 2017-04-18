a58160c37939df1e521129515d4b3cc57f90e24a
kaletvintsev <kaletvintsev@gmail.com>
2017-04-18 19:10:22 +0200

    Add files via upload

--- landing/views.py
+++ landing/views.py
@@ -6,6 +6,9 @@ from products.models import *
 
 def home(request):
     products_images = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
-    products_images_phones = products_images.filter(product__category__id=1)
-    products_images_laptops = products_images.filter(product__category__id=2)
+    products_images_on_main = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True,
+                                                  product__is_on_main=True)
+    products_images_kurtki = products_images.filter(product__category__id=1)
+    products_images_dublenki = products_images.filter(product__category__id=2)
+    products_images_plashi = products_images.filter(product__category__id=3)
     return render(request, 'landing/home.html', locals())
--- products/views.py
+++ products/views.py
@@ -4,5 +4,7 @@ from products.models import *
 
 def product(request, product_id):
     product = Product.objects.get(id=product_id)
-
     return render(request, 'products/product.html', locals())
+#
+# # def last_four(request):
+#     return render(request, 'landing/product_item.html', {'Product': Product})
